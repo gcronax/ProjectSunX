d4d168550adc18a54a540504d9a67af2da55b197
gcronax <guillecampher15@gmail.com>
2025-10-30 04:03:35 +0100

    a

--- app/src/main/java/com/example/projectsunx/MainActivity.kt
+++ app/src/main/java/com/example/projectsunx/MainActivity.kt
@@ -6,19 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
@@ -42,10 +51,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.projectsunx.ui.theme.ProjectSunXTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,27 +64,111 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectSunXTheme {
-                var imagenPasarela by rememberSaveable { mutableIntStateOf(0) }
+                var imagenPasarela by rememberSaveable { mutableIntStateOf(R.drawable.corona_solar) }
                 val navController = rememberNavController()
                 val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
                 val scope = rememberCoroutineScope()
                 ModalNavigationDrawer(
+
                     drawerState = drawerState,
                     drawerContent = {
                         ModalDrawerSheet(
                             modifier = Modifier.background(Color.White),
                             drawerShape = RoundedCornerShape(topEnd = 16.dp, bottomEnd = 16.dp)
                         ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.corona_solar),
-                                contentDescription = "Header",
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(200.dp)
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            HorizontalDivider()
-                            Spacer(modifier = Modifier.height(16.dp))
+                            Column (modifier = Modifier.fillMaxWidth()){
+                                Image(
+                                    painter = painterResource(id = imagenPasarela),
+                                    contentDescription = "Header",
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(350.dp)
+                                )
+                                Button(
+                                    onClick ={
+
+                                    },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color(0xFFE0D9F7)
+                                    ),
+                                    shape = RoundedCornerShape(16.dp),
+                                    modifier = Modifier
+                                        .height(50.dp)
+                                        .fillMaxWidth().padding(horizontal = 16.dp)
+                                ) {
+                                    Row(horizontalArrangement = Arrangement.Start,
+//                                        horizontalArrangement = Arrangement.SpaceBetween
+                                    ) {
+                                        Icon(
+                                        imageVector = Icons.Default.Build,
+                                        contentDescription = "Build",
+                                        tint = Color.Black
+                                        )
+                                        Spacer(modifier = Modifier.width(8.dp))
+                                        Text(
+                                            text = "Build",
+                                            fontSize = 16.sp,
+                                            color = Color.Black
+                                        )
+                                    }
+
+                                }
+                                Spacer(modifier = Modifier.height(16.dp))
+                                Button(
+                                    onClick ={
+
+                                    },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color(0xFFE0D9F7)
+                                    ),
+                                    shape = RoundedCornerShape(16.dp),
+                                    modifier = Modifier
+                                        .height(50.dp)
+                                        .fillMaxWidth().padding(horizontal = 16.dp)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Info,
+                                        contentDescription = "Info",
+                                        tint = Color.Black
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(
+                                        text = "Info",
+                                        fontSize = 16.sp,
+                                        color = Color.Black
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(16.dp))
+                                Button(
+                                    onClick ={
+
+                                    },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color(0xFFE0D9F7)
+                                    ),
+                                    shape = RoundedCornerShape(16.dp),
+                                    modifier = Modifier
+                                        .height(50.dp)
+                                        .fillMaxWidth().padding(horizontal = 16.dp)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Email,
+                                        contentDescription = "Email",
+                                        tint = Color.Black
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(
+                                        text = "Email",
+                                        fontSize = 16.sp,
+                                        color = Color.Black
+                                    )
+                                }
+
+
+
+
+
+                            }
                         }
                     },
 
@@ -85,9 +180,13 @@ class MainActivity : ComponentActivity() {
                                 contentColor = Color.White,
                                 actions ={
                                     IconButton(onClick = {
-
-
-
+                                        scope.launch {
+                                            if (drawerState.isClosed) {
+                                                drawerState.open()
+                                            } else {
+                                                drawerState.close()
+                                            }
+                                        }
                                     }) {
                                         Icon(
                                             Icons.AutoMirrored.Filled.ArrowBack,
